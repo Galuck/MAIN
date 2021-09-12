27a550a6b0927bb539e4e72cd90f5fe3112ab21d
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2021-09-12 16:53:05 -0300

    .

---
+++ Java/General_Study/StringConditions.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package intro_algoritmos;
+
+/**
+ * @author Gabriel May
+ *
+ */
+public class StringConditions {
+
+	
+	public static void main(String[] args) {
+		
+		String nome = "Gabriel May";
+		
+		//faz o papel do string.h do c
+		if(nome.equals("Gabriel May")) {
+			System.out.println(nome);
+		}
+		
+	}
+
+}
