2dfa0161392caf6775062adc4e13612eeb87f5c8
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2021-09-13 18:15:43 -0300

    ,

---
+++ Python/Básico/lists.py
@@ -0,0 +1,16 @@
+lista= ["Kevin", 2, 5.5, True, False]
+#         0      1   2     3     4     INDEX
+print (lista)#vai printar tudo na lista
+
+print(lista[3], "  #vai printar apenas o terceiro elemento, true")
+print(lista[4])
+print(lista[2])
+print(lista[0], "  #como em basicamente toda linguagem, começa no zero")
+
+print(lista[-1], "  #com sinal negativo, printa de trás pra frente")
+print(lista[-2])
+
+print(lista[2:], "  #vai printar tudo do2 pra frete")
+print(lista[0:3])#printa só do 0 ao 2, até o 3, mas n inclui o 3
+
+#colocar lists functions depois
