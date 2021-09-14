590b451ad89c476f73719522e49f993c567beff4
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2021-09-14 18:58:31 -0300

    tuples

---
+++ Python/Básico/tuple.py
@@ -0,0 +1,8 @@
+#tuples 
+
+coordenadas =(4,5)  #tuples são imutáveis, não tem como mudar uma tuple depois de declarada
+#a mesma coisa que lista, só que não muda
+
+#é, é isso
+
+listaDeCoordenadas=[(4,6), (8,9), (10,129)]# pode fazer também listas com essa tuples
