eea038792c96739f91b9476b82ba2cf5b6dbee04
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2021-09-14 19:16:32 -0300

    functions

---
+++ Python/Básico/functions.py
@@ -0,0 +1,16 @@
+#funçoes, como fazer uma?
+
+#def para definir a função
+def say_Hi():          
+    #identar para ficar dentro da função
+    name = input("Type your name!\n")
+    print("Hi", name)
+
+say_Hi()#chamar a função
+
+#resto funciona quase igual ao C
+
+def mostrar_soma(a,b):
+    print(int(a)+int(b))
+
+mostrar_soma(4,5)#printou 9
