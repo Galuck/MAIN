d8710062dd001a21789e60a87b88d5de57befdc6
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2021-09-15 14:58:47 -0300

    functions

--- Python/Básico/functions.py
+++ Python/Básico/functions.py
@@ -14,3 +14,14 @@ def mostrar_soma(a,b):
     print(int(a)+int(b))
 
 mostrar_soma(4,5)#printou 9
+
+
+#return
+
+def elevar_numero_ao_cubo(a):
+    resultado= a*a*a
+    return resultado #assim como no C, após o return ser chamado, a função fecha
+
+resultado2=elevar_numero_ao_cubo(2)
+
+print(resultado2)
