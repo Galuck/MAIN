0a4ee88b85c1c30b6127087ac737b53ed353cd9b
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2021-09-16 18:56:00 -0300

    pilha00

---
+++ C/pilhaArray.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define tamanho 5
+#define vazio -1
+//pilha estática (com array)
+int pilha [tamanho];
+int topo= vazio;
+
+
+int push(int valor){
+
+    if(topo>=tamanho-1){
+        return 0;  //cheio
+    }
+    topo++;
+    pilha[topo]=valor;
+    return 1;//incluido
+}
+
+int pop(int valor){
+    if(topo==vazio){
+        return 0;
+    }
+
+    int aux= pilha[topo];
+    topo--;
+    return 1;
+}
+
+
+//structs//
+int main(){
+    push(69);
+    push(420);
+    push(666);
+    push(45);
+    push(55);
+    
+    printf("(%i)\n", pilha[4]);
+    printf("(%i)\n", pilha[3]);
+    printf("(%i)\n", pilha[2]);
+    printf("(%i)\n", pilha[1]);
+    printf("(%i)\n", pilha[0]);
+
+
+    printf("\n");
+    return 0;
+}
