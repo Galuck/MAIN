4ac797d42ec48e324edc9f240b672ceb740426cb
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2021-09-18 15:59:09 -0300

    .

---
+++ Python/Básico/areaCirculo.py
@@ -0,0 +1,5 @@
+#  Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio =float(input ("\nDigite o tamanho do Raio do círculo em cm: \n")) 
+area= 2*(raio*raio)
+print("\nA área do circulo é igual a: "+ str(area)+"pi\n") #str= string
