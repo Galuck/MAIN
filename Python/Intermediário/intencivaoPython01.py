2eb5380ba8f9461bec366577165cb7fe06ea6086
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2021-10-06 14:46:48 -0300

    Aula 1 Intensivão de Python, Jupyter

--- Python/Intermediário/intencivaoPython01.py
+++ Python/Intermediário/intencivaoPython01.py
@@ -5,6 +5,7 @@ import time
 
 import sys 
 
+import pandas
 
 time.sleep(2)# ou pyautogui.PAUSE=1
 
@@ -16,7 +17,9 @@ time.sleep(1)
 
 
 #pyautogui.click()
-pyautogui.write("https://www.youtube.com/channel/UCfqMXtV0-8uVFE6tfrmQTXw") #entra no link # não escreve caracer especial como "?", usar pyperclip
+#pyautogui.write("https://www.youtube.com/channel/UCfqMXtV0-8uVFE6tfrmQTXw"#entra no link # não escreve caracer especial como "?", usar pyperclip
+pyperclip.copy("https://www.youtube.com/channel/UCfqMXtV0-8uVFE6tfrmQTXw")
+pyautogui.hotkey("ctrl", "v")
 pyautogui.press("enter") #para apenas uma tecla
 time.sleep(2) #pra dar tempo de pausar meu vídeo 
 pyautogui.press("space") #para pausar o vídeo ta página
@@ -50,6 +53,36 @@ pyautogui.click(x=1465, y=590)#download
 #ime.sleep(2)
 #print(pyautogui.position())
 
+time.sleep(7)#esperar download terminar
 
 #Calcular Indicadores
 # PANDAS    Analise de dados
+
+#as pd   pd.nlknmln
+                          #r                    #nome arquivo
+tabela = pandas.read_excel("D://Downloads OPERA GX/Vendas - Dez.xlsx")#passar nome completo do arquico com caminho (path)
+                                                    # , sheets para mudar de aba do arquivo
+display(tabela)
+                                    #média
+faturamento = tabela["Valor Final"].mean() #pegar coluna e fazer a média
+quantidade = tabela["Quantidade"].sum()#.sum vai somar
+faturamento1 = tabela["Valor Final"].sum()
+
+#calcular faturamento e qtd de produtos
+time.sleep(5)
+
+pyautogui.press("win")#agora para escrever o resultado no bloco de notas, poderia ser em qualquer lugar
+pyautogui.write("bloco de notas")
+time.sleep(0.5)
+pyautogui.press("enter")
+time.sleep(2)
+
+texto = f"""Bom dia a todos
+        A média dos preços de venda do mês é: R${faturamento:,.2f}
+        A soma das vendas do mês é: R${faturamento1:,.2f}
+        E a quantidade de produtos vendidos foi: {quantidade:,} unidades
+    
+    Grato, Gabriel May"""
+
+pyperclip.copy(texto)
+pyautogui.hotkey("ctrl", "v")
