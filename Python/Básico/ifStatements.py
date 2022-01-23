1d848219d49a353dfd5f641496228b01d114280c
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2022-01-23 14:24:26 -0300

    Update ifStatements.py

--- Python/Básico/ifStatements.py
+++ Python/Básico/ifStatements.py
@@ -22,3 +22,19 @@ elif is_male and not isTall: ## else if = elif
     print("print")
 elif not is_male or is_male==False:
     print("x")
+
+
+#################
+print ("\nMais if statements")
+
+def max_num(num1, num2, num3):
+    if num1  >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2  >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(2,3,4),"é o maio valor")
+print(max_num(8,5,3),"é o maio valor")
+print(max_num(24,30,6),"é o maio valor")
