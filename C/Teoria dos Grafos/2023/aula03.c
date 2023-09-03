33116d4c33c8c5a61be88b898b14be2235da0406
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2023-09-03 09:50:22 -0300

    aula03

---
+++ C/Teoria dos Grafos/2023/aula03.c
@@ -0,0 +1,46 @@
+//SSSP
+
+//  calcular as distancias
+//escolher vértice de origem e setar a distância dele como -1, porque quando ler a distância dele pra ele mesmo vai ser 0
+//a distância de um ponto vai ser sempre a distância do pai +1 (cada vértice vai ter a sua distância)
+//fazer isso tudo ainda com o DFS, mas agora o pai vai ajudar a medir as distâncias
+
+//void dfs(int vertice, int distancia){//SÓ PRA ÁRVORE
+
+//  visitados[vertice]=1            //atualiza o vertice novo como visitado
+//  distancias[vertice]=distancia   //atualiza a distância de cada vértice
+//  for(lista.filho){//percorrer lista de adjacencias
+//      if(filho!visitado){//se o filho não foi visitado
+//          dfs(filho,distancia+1);
+//      }
+
+//  }
+
+    
+//}
+
+
+
+//BIPARTIDO
+
+//vertices vão ser classificados em dois tipo  (qualquer coisa, vermelho e azul, sla)
+//formando dois conjuntos de vértices(cada um dos vértices pertence a um dos dois conjuntos)
+
+//qualquer aresta de um grafo bipartido TEM que conectar vertices de conjuntos DIFERENTES (conectam azul com vermelho)
+//vai definir os conjuntos percorrendo a lista de adjacência e ir alternando as cores a cada vértice (fodase a primeira cor, tanto faz)
+//(escolher raiz tbm)
+//percorrendo a lista:
+//o vértice já foi visitado?-> a cor é igual ou diferente da cor do vértice pai? se for diferente, segue, se igual, grafo n é bipartido
+
+//VAI TER QUESTAO NA PROVA PRA SABER SE É BIPARTIDO OU NÃO
+
+//na chamada da função multiplicar por -1 pra ir alternando entre as cores (azul e vermelho)
+
+
+
+
+//BFS
+
+//percorrer lista de adjacencia inteira antes de ir pro próximo (diferente do BFS que vai pulando de lista em lista)
+//enquanto percorre a lista, vai colocando os itens na fila
+//incluir todos os não visitados na fila enquanto percorre a lista de adjacência da Raiz
